b08515af5ba655bf33ff0026822ef6656a6ca48f
Alex Johnson <alex.johnson293@gmail.com>
2023-02-27 14:30:24 -0800

    remove bloat, add TopStories component

---
+++ src/components/TopStories.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function TopStories() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [topStories, setTopStories] = useState([]);
+}
+
+  useEffect(() => {
+    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+        setTopStories(jsonifiedResponse.results)
+        setIsLoaded(true);
+      })
+      
+  })
+
+
+export default TopStories;
